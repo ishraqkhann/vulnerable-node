e92179bae92d418da9249c2e31906db7139b5dcb
Ishraq Khan <38451483+ishraqkhann@users.noreply.github.com>
2025-07-14 05:44:37 -0400

    Fix 10 issues in routes/login.js

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,15 @@
-var log4js = require("log4js");
-var url = require("url");
-var express = require('express');
+const log4js = require("log4js");
+const url = require("url");
+const express = require('express');
 var auth = require("../model/auth");
-var router = express.Router();
+const router = express.Router();
 
 var logger = log4js.getLogger('vnode')
 
 // Login template
 router.get('/login', function(req, res, next) {
 
-    var url_params = url.parse(req.url, true).query;
+    const url_params = url.parse(req.url, true).query;
 
     res.render('login', {returnurl: url_params.returnurl, auth_error: url_params.error});
 });
@@ -18,6 +18,12 @@ router.get('/login', function(req, res, next) {
 // Do auth
 router.post('/login/auth', function(req, res) {
 
+    const user = req.body.username;
+router.post('/login/auth', function(req, res) {
+
+    const user = req.body.username;
+    const password = req.body.password;
+    const returnurl = req.body.returnurl;
     var user = req.body.username;
     var password = req.body.password;
     var returnurl = req.body.returnurl;
